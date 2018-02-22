120e5dda66a4cdd74f5cb074c0717d408681ac98
Reza Seyf <reza@seyf.fr>
2018-02-23 01:12:07 +0330

    index and page controller routes has been written

--- src/Cheene/FrontendBundle/Controller/DefaultController.php
+++ src/Cheene/FrontendBundle/Controller/DefaultController.php
@@ -2,16 +2,22 @@
 
 namespace Cheene\FrontendBundle\Controller;
 
+use Cheene\UserBundle\Annotation\FrontendAccessible;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="frontend_index")
+     * @FrontendAccessible(guestAccessible=true)
+     * @Method("GET")
+     * @Template()
      */
     public function indexAction()
     {
-        return $this->render('CheeneFrontendBundle:Default:index.html.twig');
+        return [];
     }
 }
--- src/Cheene/FrontendBundle/Controller/PagesController.php
+++ src/Cheene/FrontendBundle/Controller/PagesController.php
@@ -2,9 +2,22 @@
 
 namespace Cheene\FrontendBundle\Controller;
 
+use Cheene\UserBundle\Annotation\FrontendAccessible;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class PagesController extends Controller
 {
+    /**
+     * @Route("/", name="frontend_page_index")
+     * @FrontendAccessible(guestAccessible=true)
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
 }
