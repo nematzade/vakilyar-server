7401930ae9266d2fef5c3820b132984347e6a36d
Reza Seyf <rseyf2017@gmail.com>
2018-04-12 16:35:39 +0430

    - Contact Entity Changed - Slider Datatable changed

--- src/Cheene/ContentBundle/Entity/Contact.php
+++ src/Cheene/ContentBundle/Entity/Contact.php
@@ -21,6 +21,29 @@ class Contact
      */
     private $id;
 
+    /**
+     * @var string
+     * @ORM\Column(name="fullname",type="string")
+     */
+    private $fullname;
+
+    /**
+     * @var string
+     * @ORM\Column(name="email",type="string")
+     */
+    private $email;
+
+    /**
+     * @var string
+     * @ORM\Column(name="telephone",type="string")
+     */
+    private $telephone;
+
+    /**
+     * @var string
+     * @ORM\Column(name="comment",type="string")
+     */
+    private $comment;
 
     /**
      * Get id
@@ -31,5 +54,100 @@ class Contact
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set fullname
+     *
+     * @param string $fullname
+     *
+     * @return Contact
+     */
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get fullname
+     *
+     * @return string
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set telephone
+     *
+     * @param string $telephone
+     *
+     * @return Contact
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * Get telephone
+     *
+     * @return string
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return Contact
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
